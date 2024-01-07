3b45309a0d88aae8b65550081a12987d4794152c
Martynas <martynas.bartnykas@gmail.com>
2024-01-07 21:19:48 +0200

    Final task

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import java.util.OptionalInt;
+
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+
+        first = first ^ second;
+        second = first ^ second;
+        first = first ^ second;
+
+        System.out.println(first);
+        System.out.println(second);
     }
 }
